35cfd66e5b5aca0cd8a97bdae830f10d5c7ca17d
Marina-Starikovskaja <marina.starikovskaja@gmail.com>
2023-03-01 16:40:32 +0200

    убирает сомменты

--- js/filters.js
+++ js/filters.js
@@ -1,34 +1,21 @@
-/*
- * 1. Открыть и закрыть по кнопке
- * 2. Закрыть по клику в бекдроп: onBackDropClick
- * 3. Закрыть по ESC: onEscapeKeypress
- *
- * В CSS есть класс show-modal, который необходимо добавить на body при открытии модалки
- */
 
 
 const openStepBtn = document.querySelector('[data-action="open-step-2"]');
 const closeStepBtn = document.querySelector('[data-action="close-modal"]');
-// const step2 = document.querySelector('[data-action="step2"]');
-  // backdrop: document.querySelector('.js-backdrop'),
+
 
 
 openStepBtn.addEventListener('click', onOpenStep2);
 closeStepBtn.addEventListener('click', onCloseStep);
 
-// refs.backdrop.addEventListener('click', onBackdropClick);
 
 function onOpenStep2() {
-  // window.addEventListener('keydown', onEscKeyPress);
   document.body.classList.add('show-step');
-  // document.body.classList.add('hide-box')
 }
 
 
 function onAddStep() {
-  // window.removeEventListener('keydown', onEscKeyPress);
   document.body.classList.remove('show-step');
-  // document.body.classList.remove('hide-box');
 }
 
 function onCloseStep(event) {
@@ -37,20 +24,6 @@ function onCloseStep(event) {
     onAddStep();
   }
 }
-
-// function onEscKeyPress(event) {
-//   const ESC_KEY_CODE = 'Escape';
-//   const isEscKey = event.code === ESC_KEY_CODE;
-
-//   if (isEscKey) {
-//     onCloseModal();
-//   }
-// }
-
-
-// setOutput();
-
-
 
 
 
